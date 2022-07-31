36cd3e713218cc5f1daa56822ebc1f55a14ba990
Saurabh Sah <105024791+saurabhsaah@users.noreply.github.com>
2022-07-31 13:23:40 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+echo "User Id =>	" . $uid=$_GET['uid'] . "<br>";
+echo "Password =>	" . $pass=$_GET['pass'] . "<br>";
+echo "Mobile Number =>	" . $mob=$_GET['mob'] . "<br>";
+echo "Email Id =>	" . $email=$_GET['email'] . "<br>";
+?>
